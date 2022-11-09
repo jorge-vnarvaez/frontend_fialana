b9d19dee24e8b6de24c9fb1ecb6b6f87583b8f43
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-09 15:32:12 -0300

    add: postulacion component

---
+++ components/Postulacion.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-neutral-900" id="section_encuesta">
+    <v-img
+      src="/ovejas.jpg"
+      gradient="rgba(23, 23, 23, .85), rgba(23,23,23, .85)"
+      height="100vh"
+    >
+      <v-container
+        class="flex flex-col align-center justify-center text-white"
+        fill-height
+      >
+        <span class="text-3xl lg:text-5xl text-center">¿No lo sabías?</span>
+        <span class="text-3xl lg:text-5xl text-center"
+          >¡Postula tu proyecto!</span
+        >
+
+        <span class="block my-8 text-sm lg:text-xl lg:w-7/12 text-center">
+          Para enfrentar los desafíos regionales de sector necesitamos de
+          talento y el ímpetu de Magallanes. Para esto, hemos abierto un llamado
+          público a participar del proyecto a través de iniciativas que
+          contribuyan a abordar tres problemáticas del sector.
+        </span>
+
+        <span class="text-sm lg:text-xl lg:w-7/12 text-center">
+          De esta forma, te ayudaremos a través de nuestros talleres formativos
+          a desarrollar y/o transformar tu idea en un proyecto sólido y capaz de
+          postular a futuros fondos concursables. Postula y sé parte de las
+          iniciativas del futuro en el sector.
+        </span>
+
+        <button
+          class="
+            outline outline-2 outline-green-500
+            px-5
+            mt-16
+            text-sm
+            lg:text-base
+            py-1
+            rounded-full
+            text-uppercase
+            lg:font-medium
+            hover:bg-green-500
+          "
+        >
+          <a href="https://form.typeform.com/to/WVRanU7C" target="_blank"
+              ><span class="text-white">POSTULAR MI PROYECTO</span></a
+            >
+        </button>
+
+        <!-- <span class="text-sm lg:text-xl mt-6"
+          >¡Muchas gracias por tu apoyo!</span
+        > -->
+      </v-container>
+    </v-img>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -45,7 +45,7 @@
           <button
             class="outline outline-2 outline-green-500 px-5 text-sm lg:text-base py-1 rounded-full text-uppercase lg:font-medium hover:bg-green-500 mt-8"
           >
-            <a href="https://form.typeform.com/to/WVRanU7C" target="_blank"
+            <a href="#postulacion_proyecto"
               ><span class="text-white">POSTULAR MI PROYECTO</span></a
             >
           </button>
@@ -103,47 +103,7 @@
     <!-- [PRIMERA SECCION] -->
 
     <!-- [LLAMADO A LA ACCION] -->
-    <div class="bg-neutral-900" id="section_encuesta">
-      <v-img
-        src="/ovejas.jpg"
-        gradient="rgba(23, 23, 23, .85), rgba(23,23,23, .85)"
-        height="100vh"
-      >
-        <v-container
-          class="flex flex-col align-center justify-center text-white"
-          fill-height
-        >
-          <span class="text-3xl lg:text-5xl text-center">¿No lo sabías?</span>
-          <span class="text-3xl lg:text-5xl text-center"
-            >¡Participa con tu opinión!</span
-          >
-
-          <span class="block my-8 text-sm lg:text-xl lg:w-5/12 text-center">
-            Tu participación y la información que nos puedas proporcionar con
-            tus respuestas es sumamente valiosa para la etapa de diagnostico y
-            para toda la ejecución del proyecto. Por eso, te invitamos a
-            responder la siguiente encuesta.
-          </span>
-
-          <span class="text-sm lg:text-xl lg:w-5/12 text-center">
-            Contesta este instrumento no te tomará más allá de 8 minutos y todas
-            las respuestas serán de carácter confidencial.
-          </span>
-
-          <button
-            class="outline outline-2 outline-green-500 px-5 mt-16 text-sm lg:text-base py-1 rounded-full text-uppercase lg:font-medium hover:bg-green-500"
-          >
-            <a href="https://form.typeform.com/to/f2VBNq0w?" target="_blank"
-              ><span class="text-white">Responder encuesta</span></a
-            >
-          </button>
-
-          <span class="text-sm lg:text-xl mt-6"
-            >¡Muchas gracias por tu apoyo!</span
-          >
-        </v-container>
-      </v-img>
-    </div>
+    <Postulacion id="postulacion_proyecto" />
     <!-- [LLAMADO A LA ACCION] -->
 
     <!-- [NOTICIAS Y EVENTOS] -->
@@ -263,10 +223,11 @@
 import CalendarioEventos from "../components/CalendarioEventos.vue";
 import CardContenido from "../components/CardContenido.vue";
 import VideoPlayer from "../components/VideoPlayer.vue";
+import Postulacion from "../components/Postulacion.vue";
 
 export default {
   layout: "default",
-  components: { CardContenido, CalendarioEventos, VideoPlayer },
+  components: { CardContenido, CalendarioEventos, VideoPlayer, Postulacion },
   data() {
     return {
       validado: false,
